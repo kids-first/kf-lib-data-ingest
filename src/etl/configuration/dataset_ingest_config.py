c75f2407f492321cc4e6979cffd31b6f961ef84d
Natasha Singh <dukedesi22@gmail.com>
2018-07-27 13:08:24 -0400

    :sparkles: Add logging params to schema and config cls

--- src/etl/configuration/dataset_ingest_config.py
+++ src/etl/configuration/dataset_ingest_config.py
@@ -19,6 +19,11 @@ class DatasetIngestConfig(YamlConfig):
     # there and to prevent from loading subsequently.
 
     def __init__(self, config_path):
+        """
+        Construct a DatasetIngestConfig object from a config file
+
+        :params config_filepath: Path to the data ingest config file
+        """
         # Is config path a dir or file
         config_path = os.path.abspath(os.path.expanduser(config_path))
         if os.path.isdir(config_path):
@@ -31,15 +36,35 @@ class DatasetIngestConfig(YamlConfig):
         self._deserialize()
 
     def _deserialize(self):
+        """
+        Deserialize the dictionary of parameters to attributes of the
+        DatasetIngestConfig class
+        """
+        # List of entity types that should be loaded into target service
         self.target_service_entities = self.contents.get(
             'target_service_entities')
+
+        # Directory containg the extract config files
         self.extract_config_dir = os.path.join(
             os.path.dirname(self.config_filepath),
             self.contents.get('extract_config_dir'))
+
+        # List of extract config file paths
         self.extract_config_paths = [os.path.join(self.extract_config_dir,
                                                   filename)
                                      for filename in os.listdir(
                                      self.extract_config_dir)
                                      if filename.endswith('.py')]
+        # Log params
+        for param in ['log_dir', 'log_level', 'overwrite_log']:
+            if ('logging' in self.contents and
+                    (param in self.contents['logging'])):
+                setattr(self, param, self.contents['logging'][param])
+            else:
+                setattr(self, param, None)
+
+        # Study parameters
         self.study = self.contents.get('study')
+
+        # Investigator parameters
         self.investigator = self.contents.get('investigator')
