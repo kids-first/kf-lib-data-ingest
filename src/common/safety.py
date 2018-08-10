45769fc6a33002be5c0c22dd62a0a5ac2e449779
Avi Kelman <patcherton.fixesthings@gmail.com>
2018-08-10 17:42:41 -0400

    :sparkles: add safety funcs for e.g. type checks

---
+++ src/common/safety.py
@@ -0,0 +1,63 @@
+import inspect
+
+
+def function(x):
+    """
+    Lets you use `function` as a type in type_check and type_assert.
+    """
+    return inspect.isfunction(x) or inspect.isbuiltin(x)
+
+
+_basic_types = {int, float, bool, str, bytes}
+
+
+def type_check(val, *safe_types):
+    """
+    Check if val is one of the declared safe types.
+    Calling Example:
+        type_check(my_val, int, float)  # checks if my_val is an int or float
+
+    :param val: some value
+    :param *args: type classes or truthy-returning functions
+                  e.g. int, str, callable
+    :returns: True/False depending on whether val is a safe type
+    """
+    types = []
+    val_type = type(val)
+    for t in safe_types:
+        if inspect.isfunction(t) or inspect.isbuiltin(t):
+            if t(val):
+                return True
+        elif t in _basic_types:
+            if val_type is t:
+                return True
+        else:
+            if t is None:
+                t = type(None)
+            types.append(t)
+    return isinstance(val, tuple(types))
+
+
+def type_assert(name, val, *safe_types):
+    """
+    Raise an exception of val is not one of the declared safe types.
+    Calling Example:
+        type_assert(my_func, function)  # my_func must be a function
+        type_assert(my_val, int, float)  # my_val must be int or float
+
+    :raises: TypeError if type_check(val, safe_types) returns False
+    """
+    assert safe_types
+    if not type_check(val, *safe_types):
+        stack = inspect.stack()
+        caller = stack[1]
+        type_names = tuple(
+            [t.__name__ if t is not None else "None" for t in safe_types]
+        )
+        # TODO: look into getting the variable name automatically
+        # We shouldn't have to pass it in. It has to be available in the
+        # call stack.
+        raise TypeError(
+            "{}:{} requires {} to be one of {}"
+            .format(caller.filename, caller.function, name, type_names)
+        )
---
+++ tests/test_safety.py
@@ -0,0 +1,66 @@
+"""
+Tests for src/common/safety.py
+"""
+
+import pandas
+import pytest
+from common.safety import type_assert, function
+
+# values and the type of value that they are
+type_exemplars = [
+    (5, int),
+    (5.1, float),
+    ("5", str),
+    (b"5", bytes),
+    (lambda x: x, function),
+    (True, bool),
+    (False, bool),
+    (0, int),
+    (1, int),
+    (0.0, float),
+    (1.0, float),
+    (None, None),
+    (pandas.DataFrame(), pandas.DataFrame),
+    (pandas.Series(), pandas.Series)
+]
+
+
+def test_single_good_single_bad_type_checking():
+    """
+    Test all combinations of values and types in type_exemplars and make sure
+    that TypeError is raised if the val is not matched with its approved type.
+    """
+    # single good / single bad checks
+    for k, v in type_exemplars:
+        for _, w in type_exemplars:
+            if v is w:
+                type_assert(str(k), k, v)
+            else:
+                with pytest.raises(TypeError):
+                    type_assert(str(k), k, w)
+
+
+def test_complex_bad_type_checking():
+    """
+    Test each value in type_exemplars against all types except its own and make
+    sure that TypeError is raised each time.
+    """
+    for k, v in type_exemplars:
+        to_compare = []
+        for l, w in type_exemplars:
+            if v is not w:
+                to_compare.append(w)
+        with pytest.raises(TypeError):
+            type_assert(str(k), k, *to_compare)
+
+
+def test_complex_good_type_checking():
+    """
+    Test each value in type_exemplars against all the types and make sure that
+    TypeError is not raised.
+    """
+    for k, v in type_exemplars:
+        to_compare = []
+        for l, w in type_exemplars:
+            to_compare.append(w)
+        type_assert(str(k), k, *to_compare)
