546fae4490368801bf396e1d7e2c046044be1f4d
Natasha Singh <dukedesi22@gmail.com>
2018-09-17 11:25:00 -0400

    :ok_hand: DRY up file ret tests

--- tests/test_file_retriever.py
+++ tests/test_file_retriever.py
@@ -1,10 +1,8 @@
 import os
-from pprint import pprint
-
-import pytest
-import requests_mock
 
 import boto3
+import pytest
+import requests_mock
 from moto import mock_s3
 
 from common.file_retriever import FileRetriever
@@ -12,7 +10,14 @@ from conftest import TEST_DATA_DIR
 
 TEST_S3_BUCKET = "s3_bucket"
 TEST_S3_PREFIX = "mock_folder"
-TEST_FILE_PATH = os.path.join(TEST_DATA_DIR, 'data.csv')
+TEST_FILENAME = 'data.csv'
+TEST_FILE_PATH = os.path.join(TEST_DATA_DIR, TEST_FILENAME)
+TEST_PARAMS = [
+    (False, True, False),
+    (False, False, False),
+    (True, False, True),
+    (True, True, False)
+]
 
 
 @pytest.fixture(scope='session')
@@ -25,11 +30,10 @@ def storage_dir(tmpdir_factory):
     return tmpdir_factory.mktemp('retrieved_files')
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def s3_file():
     """
-    Mock bucket creation, file upload
-    Return path to file on s3
+    Mock bucket creation, file upload. Return path to file on s3.
     """
     mock_s3().start()
     # Create bucket
@@ -40,50 +44,59 @@ def s3_file():
     client.create_bucket(Bucket=TEST_S3_BUCKET)
 
     # Upload file
-    filename = 'data.csv'
-    s3_path = (f's3://{TEST_S3_BUCKET}/{TEST_S3_PREFIX}/'
-               f'{filename}')
     client.upload_file(Filename=TEST_FILE_PATH,
                        Bucket=TEST_S3_BUCKET,
-                       Key=f'{TEST_S3_PREFIX}/{filename}')
+                       Key=f'{TEST_S3_PREFIX}/{TEST_FILENAME}')
     # Sanity check - bucket exists
     response = client.head_bucket(Bucket=TEST_S3_BUCKET)
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
-    yield s3_path
+    yield f's3://{TEST_S3_BUCKET}/{TEST_S3_PREFIX}/{TEST_FILENAME}'
 
     # Teardown
     mock_s3().stop()
 
 
-@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist', [
-    (False, True, False),
-    (False, False, False),
-    (True, False, True),
-    (True, True, False)
-])
+@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist',
+                         TEST_PARAMS
+                         )
 def test_s3_file(s3_file, storage_dir, use_storage_dir, cleanup_at_exit,
                  should_file_exist):
     """
     Test file retrieval via s3
     """
-    if not use_storage_dir:
-        storage_dir = None
+    _test_get_file(s3_file, storage_dir, use_storage_dir, cleanup_at_exit,
+                   should_file_exist)
 
+
+@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist',
+                         TEST_PARAMS
+                         )
+def test_get_web(storage_dir, use_storage_dir, cleanup_at_exit,
+                 should_file_exist):
+    """
+    Test file retrieval via http
+    """
+    url = f'http://{TEST_FILENAME}'
     with open(TEST_FILE_PATH, "rb") as tf:
-        fr = FileRetriever(storage_dir=storage_dir,
-                           cleanup_at_exit=cleanup_at_exit)
-        local_copy = fr.get(s3_file, auth=None)
+        with requests_mock.Mocker() as m:
+            m.get(url, content=tf.read())
+            _test_get_file(url, storage_dir,
+                           use_storage_dir, cleanup_at_exit,
+                           should_file_exist)
 
-        tf.seek(0)
-        assert(tf.read() == local_copy.read())
-        assert_file(os.path.realpath(fr.storage_dir), local_copy.name)
 
-    # Test file existence after all refs to file obj are closed/destroyed
-    filepath = local_copy.name
-    del fr
-    del local_copy
-    assert os.path.isfile(filepath) == should_file_exist
+@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist',
+                         TEST_PARAMS
+                         )
+def test_get_local_file(storage_dir, use_storage_dir, cleanup_at_exit,
+                        should_file_exist):
+    """
+    Test file retrieval of local file
+    """
+    url = 'file://' + TEST_FILE_PATH
+    _test_get_file(url, storage_dir, use_storage_dir, cleanup_at_exit,
+                   should_file_exist)
 
 
 @pytest.mark.parametrize('url', [
@@ -91,7 +104,7 @@ def test_s3_file(s3_file, storage_dir, use_storage_dir, cleanup_at_exit,
     's3:/',
     'blah'
 ])
-def test_get_unknown(url):
+def test_invalid_urls(url):
     """
     Test bad url
     """
@@ -101,76 +114,30 @@ def test_get_unknown(url):
         assert "No client found for protocol:" in e
 
 
-@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist', [
-    (False, True, False),
-    (False, False, False),
-    (True, False, True),
-    (True, True, False)
-])
-def test_get_web(storage_dir, use_storage_dir, cleanup_at_exit,
-                 should_file_exist):
+def _test_get_file(url, storage_dir, use_storage_dir, cleanup_at_exit,
+                   should_file_exist):
     """
-    Test file retrieval via http
+    Test file retrieval
     """
     if not use_storage_dir:
         storage_dir = None
 
-    with open(TEST_FILE_PATH, "rb") as tf:
-        with requests_mock.Mocker() as m:
-            m.get('http://test', content=tf.read())
-            fr = FileRetriever(storage_dir=storage_dir,
-                               cleanup_at_exit=cleanup_at_exit)
-            local_copy = fr.get('http://test')
-
-        tf.seek(0)
-        assert(tf.read() == local_copy.read())
-        assert_file(os.path.realpath(fr.storage_dir), local_copy.name)
-
-    # Test file existence after all refs to file obj are closed/destroyed
-    filepath = local_copy.name
-    del fr
-    del local_copy
-    assert os.path.isfile(filepath) == should_file_exist
-
-
-@pytest.mark.parametrize('use_storage_dir,cleanup_at_exit,should_file_exist', [
-    (False, True, False),
-    (False, False, False),
-    (True, False, True),
-    (True, True, False)
-])
-def test_get_file(storage_dir, use_storage_dir, cleanup_at_exit,
-                  should_file_exist):
-    """
-    Test file retrieval from local machine
-    """
-    if not use_storage_dir:
-        storage_dir = None
-
-    url = 'file://' + TEST_FILE_PATH
-
-    # Open local file
     with open(TEST_FILE_PATH, "rb") as tf:
         fr = FileRetriever(storage_dir=storage_dir,
                            cleanup_at_exit=cleanup_at_exit)
-        # Get local file
         local_copy = fr.get(url)
 
-        # Test contents are as expected
-        tf.seek(0)
         assert(tf.read() == local_copy.read())
+        _assert_file(os.path.realpath(fr.storage_dir), local_copy.name)
 
-        # Test file exists in temp location
-        assert_file(os.path.realpath(fr.storage_dir), local_copy.name)
-
-    # Test file existence after all refs to file obj are closed/destroyed
-    filepath = local_copy.name
-    del fr
-    del local_copy
-    assert os.path.isfile(filepath) == should_file_exist
+        # Test file existence after all refs to file obj are closed/destroyed
+        filepath = local_copy.name
+        del fr
+        del local_copy
+        assert os.path.isfile(filepath) == should_file_exist
 
 
-def assert_file(storage_dir, file_path):
+def _assert_file(storage_dir, file_path):
     """
     Assert that storage dir and file path share same parent dir
     Assert that a file exists at file_path
