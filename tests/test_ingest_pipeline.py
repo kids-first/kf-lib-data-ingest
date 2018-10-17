b4c1ec321c1edf94dc53ec6394a640de38083fd0
Natasha Singh <dukedesi22@gmail.com>
2018-10-17 11:12:02 -0400

    :white_check_mark: Add basic CLI test

--- tests/test_ingest_pipeline.py
+++ tests/test_ingest_pipeline.py
@@ -1,10 +1,36 @@
 import os
 
+from click.testing import CliRunner
+
+import cli
 from conftest import TEST_DATA_DIR
 
+COMMAND_LINE_ERROR_CODE = 2
 
-def test(ingest_pipeline):
-    target_api_config_path = os.path.join(TEST_DATA_DIR,
-                                          'test_api_config.py')
 
-    ingest_pipeline.run(target_api_config_path)
+def test_ingest_cmd_missing_required_args():
+    """
+    Test ingest CLI when required arguments are missing
+
+    Req args: 
+        Path to dataset_ingest_config.yml or dir containing the file
+    """
+    runner = CliRunner()
+    result = runner.invoke(cli.ingest, [])
+
+    assert 'dataset_ingest_config_path'.upper() in result.output
+    assert result.exit_code == COMMAND_LINE_ERROR_CODE
+
+
+def test_ingest():
+    """
+    Test ingest runs successfully with correct params
+    """
+    path = os.path.join(TEST_DATA_DIR,
+                        'test_study', 'dataset_ingest_config.yml')
+    runner = CliRunner()
+    result = runner.invoke(cli.ingest, [path])
+
+    assert result.exit_code == 0
+    assert 'BEGIN data ingestion' in result.output
+    assert 'END data ingestion' in result.output
