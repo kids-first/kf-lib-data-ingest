700d5a031505e8271982eda3916a88e9ceefcf89
Natasha Singh <dukedesi22@gmail.com>
2018-11-05 18:18:26 -0500

    :recycle: Add unique key to concept schema
    Add unique key attribute - A unique key is value that is composed of the values from one or more
    concept attributes. It uniquely identifies concept instances of the same
    type.
    
    Change is_identifier - returns True if a concept attr is equal to
    CONCEPT|<standard concept>|UNIQUE_KEY
    
    Store the concept attributes needed to compose a unique key for each
    concept in concept_schema

--- kf_lib_data_ingest/etl/transform/standard_model/concept_schema.py
+++ kf_lib_data_ingest/etl/transform/standard_model/concept_schema.py
@@ -1,6 +1,7 @@
 from common.misc import obj_attrs_to_dict
 
 DELIMITER = '|'
+UNIQUE_ID_ATTR = 'UNIQUE_KEY'
 
 
 class FileMixin(object):
@@ -13,6 +14,7 @@ class FileMixin(object):
 
 class PropertyMixin(object):
     _CONCEPT_NAME = None
+    UNIQUE_KEY = None
     ID = None
     TARGET_SERVICE_ID = None
     HIDDEN = None
@@ -230,37 +232,89 @@ concept_set = {
 }
 
 
-def _create_identifiers():
+def _create_unique_key_composition():
     """
-    Build the set of identifying concept properties.
+    Build a dict which stores the attributes used to build a unique key
+    for a particular concept. This key uniquely identifies concept instances of
+    the same type.
 
-    - All CONCEPT.<>.ID properties are identifiers
-    - Some concepts have additional identifying properties
+    A key in the dict is a standard concept and a value in
+    the dict is a set of concept attributes.
+
+    - The ID attribute is usually populated with a unique identifier
+    provided from the source data and so the default unique key for a concept
+    is composed of just the ID attribute.
+
+    - However, some concepts don't typically have a unique ID assigned in
+    the source data. These concepts can be uniquely identified by a
+    combination of one or more other concept attributes. Thus, the unique key
+    would be composed of a set of attribute values joined together by some
+    delimiter.
+
+    - For example a phenotype doesn't typically have a unique identifier
+    assigned to it in the source data. But a phenotype can be uniquely
+    identified by a combination of the participant's id, phenotype name,
+    observed status, and the age of the participant when the observation was
+    recorded. The values for these attributes would form the unique key for a
+    phenotype instance.
     """
+    # Default unique keys
     identifiers = {}
     for concept in concept_set:
-        identifiers[concept] = {concept.ID}
+        identifiers[concept._CONCEPT_NAME] = [concept.ID]
 
-    # Add other misc identifiers
-    identifiers[CONCEPT.GENOMIC_FILE].add(CONCEPT.GENOMIC_FILE.FILE_PATH)
-    identifiers[CONCEPT.SEQUENCING].add(CONCEPT.SEQUENCING.LIBRARY_NAME)
-    identifiers[CONCEPT.BIOSPECIMEN].add(CONCEPT.BIOSPECIMEN.ALIQUOT_ID)
+    # Compound unique keys
+    identifiers[CONCEPT.INVESTIGATOR._CONCEPT_NAME] = [
+        CONCEPT.INVESTIGATOR.NAME,
+        CONCEPT.INVESTIGATOR.INSTITUTION
+    ]
+
+    identifiers[CONCEPT.DIAGNOSIS._CONCEPT_NAME] = [
+        CONCEPT.PARTICIPANT.UNIQUE_KEY,
+        CONCEPT.DIAGNOSIS.NAME,
+        CONCEPT.DIAGNOSIS.TUMOR_LOCATION,
+        CONCEPT.DIAGNOSIS.SPATIAL_DESCRIPTOR,
+        CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS
+    ]
+    identifiers[CONCEPT.PHENOTYPE._CONCEPT_NAME] = [
+        CONCEPT.PARTICIPANT.UNIQUE_KEY,
+        CONCEPT.PHENOTYPE.NAME,
+        CONCEPT.PHENOTYPE.OBSERVED,
+        CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS
+    ]
+    identifiers[CONCEPT.OUTCOME._CONCEPT_NAME] = [
+        CONCEPT.PARTICIPANT.UNIQUE_KEY,
+        CONCEPT.OUTCOME.VITAL_STATUS,
+        CONCEPT.OUTCOME.DISEASE_RELATED,
+        CONCEPT.OUTCOME.EVENT_AGE_DAYS
+    ]
+    identifiers[CONCEPT.BIOSPECIMEN_GENOMIC_FILE._CONCEPT_NAME] = [
+        CONCEPT.BIOSPECIMEN.UNIQUE_KEY,
+        CONCEPT.GENOMIC_FILE.UNIQUE_KEY
+    ]
+    identifiers[CONCEPT.BIOSPECIMEN_DIAGNOSIS._CONCEPT_NAME] = [
+        CONCEPT.BIOSPECIMEN.UNIQUE_KEY,
+        CONCEPT.DIAGNOSIS.UNIQUE_KEY
+    ]
+    identifiers[CONCEPT.READ_GROUP_GENOMIC_FILE._CONCEPT_NAME] = [
+        CONCEPT.READ_GROUP.UNIQUE_KEY,
+        CONCEPT.GENOMIC_FILE.UNIQUE_KEY
+    ]
 
     return identifiers
 
 
-# Create the set of identifier concept properties
-identifiers = _create_identifiers()
-
-
 def is_identifier(concept_property_string):
     """
-    Given a delimited concept property string check whether the property
-    is an identifying property for this concept.
-     :param concept_property_string: a string like: PARTICIPANT|ID
+    Given a delimited concept property string, check whether it is a
+    UNIQUE_KEY property. The UNIQUE_KEY property is a standard concept property
+    which is reserved to uniquely identify concept instances of the same type.
+
+    :param concept_property_string: a concept attribute string such as
+    CONCEPT|PARTICIPANT|ID
     """
 
-    return concept_property_string in set().union(*identifiers.values())
+    return concept_attr_from(concept_property_string) == UNIQUE_ID_ATTR
 
 
 def concept_from(concept_attribute_str):
@@ -275,3 +329,6 @@ def concept_attr_from(concept_attribute_str):
     Extract the concept attribute from the concept attribute string
     """
     return concept_attribute_str.split(DELIMITER)[-1]
+
+
+unique_key_composition = _create_unique_key_composition()
