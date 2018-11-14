9503d0f6b01a358ae1883b78255082246584fb9a
Natasha Singh <dukedesi22@gmail.com>
2018-11-13 20:22:45 -0500

    :recycle: Read reqs file in setup.py

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,6 @@
 
 python3 -m venv venv
 . venv/bin/activate
-pip install -r requirements.txt
+pip install -e .
 pip install -r dev-requirements.txt
 pip install -r doc-requirements.txt
-pip install -e .
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
 from setuptools import setup, find_packages
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+req_file = os.path.join(root_dir, 'requirements.txt')
+with open(req_file) as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='kf-lib-data-ingest',
@@ -6,12 +12,11 @@ setup(
     description='Kids First Data Ingest CLI',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    entry_points='''
-        [console_scripts]
-        kidsfirst=cli:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'kidsfirst=cli:cli',
+        ],
+    },
     include_package_data=True,
-    install_requires=[
-        'Click',
-    ]
+    install_requires=requirements
 )
