2a141de7e24848c1116896ff644309e0bde44c0a
Natasha Singh <dukedesi22@gmail.com>
2019-01-17 13:33:43 -0500

    :white_check_mark: Test TransformModule class

---
+++ tests/data/test_study/transform_module.py
@@ -0,0 +1,6 @@
+
+
+def transform_function(mapped_df_dict):
+    entity_dataframes = {}
+
+    return entity_dataframes
--- tests/test_guided_transform.py
+++ tests/test_guided_transform.py
@@ -1,13 +1,5 @@
-"""
-We only need to test validate run parameters since the actual transformation
-functionality is heavily tested in tests/test_standard_model.py
-"""
-import os
-
 import pytest
-import pandas as pd
 
-from kf_lib_data_ingest.common.errors import InvalidIngestStageParameters
 from kf_lib_data_ingest.etl.configuration.base_config import (
     ConfigValidationError
 )
@@ -25,18 +17,10 @@ def transform_module():
     return TransformModule(TRANSFORM_MODULE_PATH)
 
 
-def test_invalid_run_parameters(transform_stage):
-
-    # Bad keys
-    with pytest.raises(InvalidIngestStageParameters):
-        transform_stage.run({i: 'foo' for i in range(5)})
-
-    # Bad values
-    with pytest.raises(InvalidIngestStageParameters):
-        transform_stage.run({'foor': ('bar', None) for i in range(5)})
-
-
 def test_transform_module(transform_module):
+    """
+    Test validation of user supplied transform module
+    """
     # Valid transform module
     assert transform_module
 
