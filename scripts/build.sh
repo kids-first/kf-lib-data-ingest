6b851a206e37800e02a3a6f2b9a6dc0b66ddc20e
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2019-02-14 16:29:25 -0500

    :arrow_up: Install egg info first

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ set -e
 python3 -m venv venv
 . venv/bin/activate
 pip install --upgrade pip
+python setup.py install_egg_info
 pip install -e .
 pip install -r dev-requirements.txt
 pip install -r doc-requirements.txt
