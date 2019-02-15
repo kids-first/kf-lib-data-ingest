7bbf4bdac068ce8fc34e6e069b8c818edbf700e6
Natasha Singh <dukedesi22@gmail.com>
2019-02-15 15:03:39 -0500

    :sparkles: Generate ingest package from template

---
+++ kf_lib_data_ingest/factory/generate.py
@@ -0,0 +1,38 @@
+"""
+Module to keep code for generating code or files
+"""
+
+import os
+import shutil
+
+
+def new_ingest_pkg(dest_dir=None):
+    """
+    Create a new ingest package using the template defined in
+    ./templates/study
+
+    :param dest_dir: (Optional) path to the dir where ingest package will be
+    created
+    :type dest_dir: str
+    :return: dest_dir
+    """
+    # Make destination dir path
+    if not dest_dir:
+        dest_dir = os.getcwd()
+        dest_dir = os.path.join(dest_dir, 'my_study')
+    else:
+        # Expand . and ~
+        dest_dir = os.path.expanduser(dest_dir)
+        dest_dir = os.path.realpath(dest_dir)
+
+    # Don't overwrite existing ingest dirs
+    if os.path.isdir(dest_dir):
+        raise FileExistsError(
+            f'Cannot create a new ingest package here: {dest_dir}. '
+            'Directory already exists!')
+
+    # Create package
+    src_dir = os.path.join(os.path.dirname(__file__), 'templates', 'study')
+    shutil.copytree(src_dir, dest_dir)
+
+    return dest_dir
