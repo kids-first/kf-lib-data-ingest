d81c8dd73a5f9db1bab81c80390d656f5bf1c08f
Natasha Singh <dukedesi22@gmail.com>
2019-02-15 15:02:53 -0500

    :sparkles: Add ingest package template

---
+++ kf_lib_data_ingest/factory/templates/study/extract_configs/extract_config.py
@@ -0,0 +1,23 @@
+"""
+Auto-generated extract config module.
+
+See documentation at
+https://kids-first.github.io/kf-lib-data-ingest/ for information on writing
+extract config files.
+"""
+
+from kf_lib_data_ingest.common import constants
+from kf_lib_data_ingest.etl.extract.operations import *
+from kf_lib_data_ingest.etl.transform.standard_model.concept_schema import (
+    CONCEPT
+)
+
+# TODO Fill in valid URL to source data file here
+source_data_url = ''
+
+# TODO (Optional) Fill in special loading parameters here
+source_data_loading_parameters = {}
+
+
+# TODO Fill in extract operations here
+operations = []
---
+++ kf_lib_data_ingest/factory/templates/study/transform_module.py
@@ -0,0 +1,19 @@
+"""
+Auto-generated transform module
+
+Replace the contents of transform_function with your own code
+
+See documentation at
+https://kids-first.github.io/kf-lib-data-ingest/ for information on
+implementing transform_function.
+"""
+
+import pandas as pd
+
+
+def transform_function(mapped_df_dict):
+    entity_dataframes = {
+        'participant': pd.DataFrame()
+    }
+
+    return entity_dataframes
