1eb24371f80b95c6abe8fbba59a63ff27644920e
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-02-27 11:16:35 -0500

    :white_check_mark: add test

---
+++ tests/test_misc.py
@@ -0,0 +1,22 @@
+import pandas
+
+from kf_lib_data_ingest.common.misc import *
+
+
+def test_to_str_with_floats_downcast_to_ints_first():
+    df1 = pandas.DataFrame(
+        {'a': [1, 2, 3, None], 'b': ['1_1', ' ab ', 2, None]}, dtype=object
+    )
+    df2 = pandas.read_json(df1.to_json(), dtype=object)
+    assert not df1.astype(str).equals(df2.astype(str))
+
+    df1 = df1.applymap(
+        lambda x: to_str_with_floats_downcast_to_ints_first(x, replace_na=True)
+    )
+    df2 = df2.applymap(
+        lambda x: to_str_with_floats_downcast_to_ints_first(x, replace_na=True)
+    )
+    assert df1.astype(str).equals(df2.astype(str))
+
+    assert df1['b'][0] == '1_1'
+    assert df1['b'][1] == 'ab'
