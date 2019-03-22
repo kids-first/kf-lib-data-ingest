a533ba6ad298ac4a8df83650731b5dcb901cdb43
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2019-03-22 15:14:08 -0400

    :bug: The dataservice throws 500 if gen3 does
    Gen3 throws internal errors if you ask it to walk and chew gum at the same time. 
    Then dataservice forwards the 500 status, which basically kills GF endpoint access until https://github.com/kids-first/kf-api-dataservice/issues/487 gets fixed.

--- kf_lib_data_ingest/common/misc.py
+++ kf_lib_data_ingest/common/misc.py
@@ -154,7 +154,7 @@ def upper_camel_case(snake_str):
 
 def requests_retry_session(
         session=None, total=3, read=3, connect=0, status=3,
-        backoff_factor=5, status_forcelist=(502, 503, 504)
+        backoff_factor=5, status_forcelist=(500, 502, 503, 504)
 ):
     """
     Send an http request and retry on failures or redirects
