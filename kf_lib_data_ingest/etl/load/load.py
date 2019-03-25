a01bae2eda85bffadd020b879fc700d7396fc0e3
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-03-25 15:59:28 -0400

    :recycle: move target uid lookup out of transform into load
    This reduces a little bit of complexity by removing batch code that
    can't solve the problem it needs to solve.
    
    Load needs to know how to look up UIDs on the fly, because it will
    regularly be the case that none or only some of them are prepopulated,
    and if one wasn't prepopulated then everything dependent on it will need
    to look it up to connect their foreign keys.
    
    So if Load needs to know how to do it on the fly anyway, then there's no
    value in doing it in bulk during transform.

--- kf_lib_data_ingest/etl/load/load.py
+++ kf_lib_data_ingest/etl/load/load.py
@@ -1,10 +1,14 @@
 """
 Module for loading the transform output into the dataservice.
 """
+import os
 from abc import abstractmethod
+from urllib.parse import urlparse
 
 from kf_lib_data_ingest.common.errors import InvalidIngestStageParameters
+from kf_lib_data_ingest.common.misc import read_json
 from kf_lib_data_ingest.common.stage import IngestStage
+from kf_lib_data_ingest.config import DEFAULT_ID_CACHE_FILENAME
 from kf_lib_data_ingest.etl.configuration.target_api_config import (
     TargetAPIConfig
 )
@@ -19,13 +23,29 @@ from kf_lib_data_ingest.etl.configuration.target_api_config import (
 
 class LoadStage(IngestStage):
     def __init__(
-        self, target_api_config_path, target_url, use_async, entities_to_load
+        self, target_api_config_path, target_url, entities_to_load,
+        uid_cache_dir=None, use_async=False
     ):
         super().__init__()
         self.target_api_config = TargetAPIConfig(target_api_config_path)
         self.entities_to_load = entities_to_load
         self.target_url = target_url
+        self.uid_cache_filepath = os.path.join(
+            uid_cache_dir or os.getcwd(),
+            urlparse(target_url).netloc + '_' + DEFAULT_ID_CACHE_FILENAME
+            #  Every target gets its own cache because they don't share UIDs
+        )
+        try:
+            self.uid_cache = read_json(self.uid_cache_filepath)
+        except FileNotFoundError as e:
+            self.logger.warning(
+                f'Target UID cache file not found: {self.uid_cache_filepath}'
+            )
+            self.uid_cache = {}
         self.use_async = use_async
+
+    def _unique_key_to_cached_target_id(self, entity_type, entity_unique_key):
+        return self.uid_cache.get(entity_type, {}).get(entity_unique_key)
 
     def _read_output(self):
         # An ingest stage is responsible for serializing the data that is
