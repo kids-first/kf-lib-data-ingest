4b180f59ca4bf35304a14ee4eac124e3665471d7
Natasha Singh <dukedesi22@gmail.com>
2019-04-04 10:12:48 -0400

    :whale: Add setup env in docker entrypoint

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,11 @@ RUN pip install --upgrade pip && \
     # pip install -r dev-requirements.txt && \
     pip install --no-cache-dir -e .
 
+RUN apt-get update && apt-get install -y jq wget && \
+    wget -q -O vault.zip https://releases.hashicorp.com/vault/1.0.3/vault_1.0.3_linux_amd64.zip && \
+    unzip vault.zip && \
+    mv vault /usr/local/bin
+
 COPY . /app
 
 ENTRYPOINT ["/app/scripts/entrypoint.sh"]
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $KF_INGEST_APP_MODE = "production" ]]; then
+    echo "Loading production app settings into environment ..."
+
+    if [[ -n $VAULT_ADDR ]] && [[ -n $VAULT_ROLE ]]; then
+        vault login -method=aws role=$VAULT_ROLE 2>&1 | grep authent
+
+        echo "Load secrets from vault ..."
+
+        # Get secrets from vault
+        vault_json = $(vault read -format=json "secret/kf_ingest_app")
+        secrets=$(echo $vault_json | jq -r ".data.data | to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]")
+
+        # Set env vars
+        for s in $secrets; do
+            while IFS=$'=' read -r key value
+            do
+                export "$key"="$value"
+            done <<< "$s"
+        done
+    fi
+fi
+
+kidsfirst "$@"
