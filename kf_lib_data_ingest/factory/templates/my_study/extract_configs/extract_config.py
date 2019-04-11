b7bba143bc7b34e0d1b2934fff790f63ab37a75a
Natasha Singh <dukedesi22@gmail.com>
2019-04-11 16:43:42 -0400

    :wrench: Make template pkg realistic + work

--- kf_lib_data_ingest/factory/templates/my_study/extract_configs/extract_config.py
+++ kf_lib_data_ingest/factory/templates/my_study/extract_configs/extract_config.py
@@ -12,12 +12,37 @@ from kf_lib_data_ingest.common.concept_schema import (
     CONCEPT
 )
 
-# TODO Fill in valid URL to source data file here
-source_data_url = ''
+# TODO - Replace this with a URL to your own data file
+source_data_url = 'file://../data/clinical.tsv'
 
 # TODO (Optional) Fill in special loading parameters here
 source_data_loading_parameters = {}
 
 
-# TODO Fill in extract operations here
-operations = []
+# TODO - Replace this with operations that make sense for your own data file
+operations = [
+    keep_map(
+        in_col='family',
+        out_col=CONCEPT.FAMILY.ID
+    ),
+    keep_map(
+        in_col='subject',
+        out_col=CONCEPT.PARTICIPANT.ID
+    ),
+    keep_map(
+        in_col='gender',
+        out_col=CONCEPT.PARTICIPANT.GENDER
+    ),
+    keep_map(
+        in_col='sample',
+        out_col=CONCEPT.BIOSPECIMEN.ID
+    ),
+    keep_map(
+        in_col='analyte',
+        out_col=CONCEPT.BIOSPECIMEN.ANALYTE
+    ),
+    keep_map(
+        in_col='diagnosis',
+        out_col=CONCEPT.DIAGNOSIS.NAME
+    )
+]
