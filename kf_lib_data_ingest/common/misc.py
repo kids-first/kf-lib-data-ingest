3736c083f198e0d45d8ca9a53449adcf41ec4c15
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-04-25 14:13:35 -0400

    :recycle: move timestamp function into common
    we're going to use it somewhere else now

--- kf_lib_data_ingest/common/misc.py
+++ kf_lib_data_ingest/common/misc.py
@@ -1,9 +1,11 @@
+import datetime
 import importlib
 import inspect
 import json
 import logging
 import os
 import re
+import time
 from itertools import tee
 
 import jsonpickle
@@ -347,3 +349,21 @@ def get_open_api_v2_schema(url, entity_names,
             print(err)
 
     return output
+
+
+def timestamp():
+    """
+    Helper to create an ISO 8601 formatted string that represents local time
+    and includes the timezone info.
+    """
+    # Calculate the offset taking into account daylight saving time
+    # https://stackoverflow.com/questions/2150739/iso-time-iso-8601-in-python
+    if time.localtime().tm_isdst:
+        utc_offset_sec = time.altzone
+    else:
+        utc_offset_sec = time.timezone
+    utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+    t = datetime.datetime.now().replace(
+        tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
+
+    return str(t)
