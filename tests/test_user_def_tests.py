322220322380bdcbf51ea2867eca401e344d9da2
Natasha Singh <dukedesi22@gmail.com>
2019-04-29 17:55:15 -0400

    :white_check_mark: Test user def tests

---
+++ tests/test_user_def_tests.py
@@ -0,0 +1,66 @@
+import os
+
+import pytest
+
+from conftest import make_ingest_pipeline, delete_dir_contents
+from kf_lib_data_ingest.factory.generate import new_ingest_pkg
+
+
+@pytest.fixture(scope='function')
+def ingest_pkg_dir(tmpdir):
+    ingest_pkg_dir = new_ingest_pkg(os.path.join(tmpdir, 'study1'))
+    delete_dir_contents(os.path.join(ingest_pkg_dir, 'tests'))
+    return ingest_pkg_dir
+
+
+def test_user_def_test_pass(info_caplog, ingest_pkg_dir):
+    """
+    Test successful user defined tests
+    """
+    # Create passing test file
+    filepath = os.path.join(ingest_pkg_dir, 'tests', 'test_passing.py')
+    with open(filepath, 'w') as test_file:
+        content = '''def test():
+            assert True'''
+        test_file.write(content)
+    assert os.path.isfile(filepath)
+
+    ingest_pipeline = make_ingest_pipeline(
+        config_filepath=ingest_pkg_dir,
+        log_dir=os.path.join(ingest_pkg_dir, 'logs'))
+    assert ingest_pipeline.run()
+    assert '✅  User defined data validation tests passed' in info_caplog.text
+
+
+def test_user_def_test_not_collected(info_caplog, ingest_pkg_dir):
+    """
+    Test successful pass of user defined tests even when no tests collected
+    """
+    # Run pipeline and check log
+    ingest_pipeline = make_ingest_pipeline(
+        config_filepath=ingest_pkg_dir,
+        log_dir=os.path.join(ingest_pkg_dir, 'logs'))
+    assert ingest_pipeline.run()
+    assert (f'⚠️  pytest did not collect any user defined tests' in
+            info_caplog.text)
+
+
+def test_user_def_test_fail(info_caplog, ingest_pkg_dir):
+    """
+    Test failure of user defined tests
+    """
+    # Create failing test file
+    filepath = os.path.join(ingest_pkg_dir, 'tests', 'test_passing.py')
+    with open(filepath, 'w') as test_file:
+        content = '''def test():
+            assert False'''
+        test_file.write(content)
+    assert os.path.isfile(filepath)
+
+    # Run pipeline and check log
+    ingest_pipeline = make_ingest_pipeline(
+        config_filepath=ingest_pkg_dir,
+        log_dir=os.path.join(ingest_pkg_dir, 'logs'))
+    assert not ingest_pipeline.run()
+    assert (f'❌  User defined data validation tests failed' in
+            info_caplog.text)
