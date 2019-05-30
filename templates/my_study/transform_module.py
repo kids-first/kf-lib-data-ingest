a2beb19a70ce6d5ce8da816d7919d1b879736ff8
Natasha Singh <dukedesi22@gmail.com>
2019-05-30 15:21:57 -0400

    :recycle: Template study transform func outputs df dict

--- templates/my_study/transform_module.py
+++ templates/my_study/transform_module.py
@@ -13,9 +13,44 @@ from kf_lib_data_ingest.common.pandas_utils import (
     outer_merge,
     merge_wo_duplicates
 )
+from kf_lib_data_ingest.config import DEFAULT_KEY
 
 
 def transform_function(mapped_df_dict):
-    # Expected to return a single merged dataframe
+    """
+    Merge DataFrames in mapped_df_dict into 1 DataFrame if possible.
 
-    return list(mapped_df_dict.values())[0]
+    Return a dict that looks like this:
+
+    {
+        'default': all_merged_data_df
+    }
+
+    If not possible to merge all DataFrames into a single DataFrame then
+    you can return a dict that looks something like this:
+
+    {
+        '<name of target concept>': df_for_<target_concept>,
+        'default': all_merged_data_df
+    }
+
+    Target concept instances will be built from the default DataFrame, unless
+    another DataFrame is explicitly provided via a key, value pair in the
+    output dict. They key must match the name of an existing target concept.
+    The value will be the DataFrame to use when building instances of the
+    target concept.
+
+    A typical example would be:
+
+    {
+        'family_relationship': family_relationship_df,
+        'default': all_merged_data_df
+    }
+
+    """
+
+    df = list(mapped_df_dict.values())[0]
+
+    return {
+        DEFAULT_KEY: df
+    }
