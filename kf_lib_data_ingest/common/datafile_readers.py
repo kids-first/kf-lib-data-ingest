e0fabf406977ac6ae25729c9c5a3726b2f3accd7
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-08-14 13:02:18 -0400

    :recycle: Factor out pandas read file functions
    Logic that was in the extract stage is now in datafile_readers, and the
    readers are now smarter about binary vs ascii file modes.

--- kf_lib_data_ingest/common/datafile_readers.py
+++ kf_lib_data_ingest/common/datafile_readers.py
@@ -2,6 +2,7 @@
 Contains file readers (file -> dataframe) for file types that benefit from
 extra help.
 """
+import io
 import os
 
 import pandas
@@ -9,8 +10,16 @@ import xlrd
 from pandas.api.types import is_file_like
 
 
+def __ascii_wrap(potentially_binary_buffer):
+    try:
+        return io.TextIOWrapper(potentially_binary_buffer)
+    except Exception:
+        return potentially_binary_buffer
+
+
 def read_excel_file(filepath_or_buffer, **kwargs):
-    """Return contents of an excel spreadsheet as a pandas DataFrame.
+    """
+    Return contents of an excel spreadsheet as a pandas DataFrame.
 
     :param filepath_or_buffer: an xls or xlsx spreadsheet file
     :type filepath_or_buffer: string (path) or file-like object
@@ -32,5 +41,38 @@ def read_excel_file(filepath_or_buffer, **kwargs):
             )
 
     kwargs['engine'] = 'xlrd'
-    kwargs['dtype'] = object
+    kwargs['dtype'] = str
+    kwargs['na_filter'] = False
     return pandas.read_excel(wb, **kwargs)
+
+
+def read_table_file(filepath_or_buffer, **kwargs):
+    """
+    Return contents of a delimited table file (like csv) as a pandas DataFrame.
+
+    :param filepath_or_buffer: a delimited file
+    :type filepath_or_buffer: string (path) or file-like object
+    :param **kwargs: See docs for pandas.read_csv
+    :return: The structured contents of the file
+    :rtype: pandas.Dataframe
+    """
+    kwargs['sep'] = kwargs.pop('delimiter', None) or kwargs.get('sep')
+    kwargs['engine'] = 'python'
+    kwargs['dtype'] = str
+    kwargs['na_filter'] = False
+    return pandas.read_csv(__ascii_wrap(filepath_or_buffer), **kwargs)
+
+
+def read_json_file(filepath_or_buffer, **kwargs):
+    """
+    Return contents of a json file (dict of lists or list of dicts) as a pandas
+    DataFrame.
+
+    :param filepath_or_buffer: a json file
+    :type filepath_or_buffer: string (path) or file-like object
+    :param **kwargs: See docs for pandas.read_json
+    :return: The structured contents of the file
+    :rtype: pandas.Dataframe
+    """
+    kwargs['convert_dates'] = False
+    return pandas.read_json(__ascii_wrap(filepath_or_buffer), **kwargs)
