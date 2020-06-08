fc0caad16006494124468504623d0df5c8391e06
Avi Kelman <patcherton.fixesthings@gmail.com>
2020-06-08 10:00:18 -0600

    :rotating_light: Fix new flake alerts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 black --line-length 80 kf_lib_data_ingest tests
-flake8 --ignore=E501,W503,E203 kf_lib_data_ingest tests
+flake8 --ignore=E501,W503,E203 kf_lib_data_ingest
+flake8 --ignore=E501,W503,E203,E741 tests
