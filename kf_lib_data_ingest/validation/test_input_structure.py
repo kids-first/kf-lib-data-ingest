2a6d39148c72584efc0707b48387577e8c9e9819
gsantia <gs495@drexel.edu>
2020-09-02 13:34:34 -0400

    :sparkles: complete input validation dict

--- kf_lib_data_ingest/validation/test_input_structure.py
+++ kf_lib_data_ingest/validation/test_input_structure.py
@@ -33,7 +33,7 @@ check_non_negative = lambda x: int(x) >= 0
 
 INPUT_VALIDATION = {
     # PARTICIPANT
-    CONCEPT.PARTICIPANT.RACE: lambda x: x in {
+    CONCEPT.PARTICIPANT.RACE: null_wrapper({
           constants.RACE.WHITE,
           constants.RACE.NATIVE_AMERICAN,
           constants.RACE.BLACK,
@@ -41,22 +41,22 @@ INPUT_VALIDATION = {
           constants.RACE.PACIFIC,
           constants.COMMON.OTHER,
           constants.RACE.MULTIPLE
-    },
+    }),
     CONCEPT.PARTICIPANT.ENROLLMENT_AGE_DAYS: check_age,
-    CONCEPT.PARTICIPANT.ETHNICITY: lambda x: x in {
+    CONCEPT.PARTICIPANT.ETHNICITY: null_wrapper({
         constants.ETHNICITY.HISPANIC,
         constants.ETHNICITY.NON_HISPANIC
-    },
-    CONCEPT.PARTICIPANT.GENDER: lambda x: x in {
+    }),
+    CONCEPT.PARTICIPANT.GENDER: null_wrapper({
         constants.GENDER.MALE,
         constants.GENDER.FEMALE,
         constants.COMMON.OTHER
-    },
-    CONCEPT.PARTICIPANT.SPECIES: lambda x: x in {
+    }),
+    CONCEPT.PARTICIPANT.SPECIES: null_wrapper({
         constants.SPECIES.DOG,
         constants.SPECIES.HUMAN
-    },
-    CONCEPT.PARTICIPANT.CONSENT_TYPE: lambda x: x in {
+    }),
+    CONCEPT.PARTICIPANT.CONSENT_TYPE: null_wrapper({
         constants.CONSENT_TYPE.DS_OBDR_MDS,
         constants.CONSENT_TYPE.DS_OBD_MDS,
         constants.CONSENT_TYPE.DS_OC_PUB_MDS,
@@ -65,25 +65,25 @@ INPUT_VALIDATION = {
         constants.CONSENT_TYPE.DS_CHD,
         constants.CONSENT_TYPE.DS_CHD_IRB,
         constants.CONSENT_TYPE.GRU
-    },
+    }),
 
     # BIOSPECIMEN
     CONCEPT.BIOSPECIMEN.EVENT_AGE_DAYS: check_age,
-    CONCEPT.BIOSPECIMEN.ANALYTE: lambda x: x in {
+    CONCEPT.BIOSPECIMEN.ANALYTE: null_wrapper({
         constants.SEQUENCING.ANALYTE.DNA,
         constants.SEQUENCING.ANALYTE.RNA,
         constants.SEQUENCING.ANALYTE.VIRTUAL,
         constants.COMMON.OTHER
-    },
+    }),
     CONCEPT.BIOSPECIMEN.CONCENTRATION_MG_PER_ML: check_positive,
-    CONCEPT.BIOSPECIMEN.SAMPLE_PROCUREMENT: lambda x: x in {
+    CONCEPT.BIOSPECIMEN.SAMPLE_PROCUREMENT: null_wrapper({
         constants.SPECIMEN.SAMPLE_PROCUREMENT.AUTOPSY,
         constants.SPECIMEN.SAMPLE_PROCUREMENT.BIOPSY,
         constants.SPECIMEN.SAMPLE_PROCUREMENT.SUBTOTAL_RESECTION,
         constants.SPECIMEN.SAMPLE_PROCUREMENT.TOTAL_RESECTION,
         constants.SPECIMEN.SAMPLE_PROCUREMENT.BLOOD_DRAW,
         constants.COMMON.OTHER
-    },
+    }),
     CONCEPT.BIOSPECIMEN.SHIPMENT_DATE: check_date,
     CONCEPT.BIOSPECIMEN.VOLUME_UL: check_positive,
 
@@ -93,24 +93,24 @@ INPUT_VALIDATION = {
 
     # DIAGNOSIS
     CONCEPT.DIAGNOSIS.EVENT_AGE_DAYS: check_positive,
-    CONCEPT.DIAGNOSIS.CATEGORY: lambda x: x in {
+    CONCEPT.DIAGNOSIS.CATEGORY: null_wrapper({
         constants.STUDY.CANCER,
         constants.STUDY.STRUCTURAL_DEFECT,
         constants.COMMON.OTHER
-    },
+    }),
 
     # FAMILY
+    # There's an enum for FAMILY_TYPE in the dataservice, but nothing for this
+    # exists in the ingest library. Not sure what to do here.
 
     # FAMILY_RELATIONSHIP
 
     # GENOMIC_FILE
-    CONCEPT.GENOMIC_FILE.AVAILABILITY: lambda x:
-        (x in {
+    CONCEPT.GENOMIC_FILE.AVAILABILITY: null_wrapper({
             constants.GENOMIC_FILE.AVAILABILITY.IMMEDIATE,
             constants.GENOMIC_FILE.AVAILABILITY.COLD_STORAGE
-        }) or
-        (x in NULL_VALUES),
-    CONCEPT.GENOMIC_FILE.DATA_TYPE: lambda x: (x in {
+        }),
+    CONCEPT.GENOMIC_FILE.DATA_TYPE: null_wrapper({
         constants.GENOMIC_FILE.DATA_TYPE.ALIGNED_READS,
         constants.GENOMIC_FILE.DATA_TYPE.ALIGNED_READS_INDEX,
         constants.GENOMIC_FILE.DATA_TYPE.EXPRESSION,
@@ -131,39 +131,39 @@ INPUT_VALIDATION = {
         constants.GENOMIC_FILE.DATA_TYPE.UNALIGNED_READS,
         constants.GENOMIC_FILE.DATA_TYPE.VARIANT_CALLS,
         constants.GENOMIC_FILE.DATA_TYPE.VARIANT_CALLS_INDEX
-    }) or (x in NULL_VALUES),
-    CONCEPT.SEQUENCING.PAIRED_END: lambda x: (x in {1, 2}) or (x in NULL_VALUES),
+    }),
+    CONCEPT.SEQUENCING.PAIRED_END: null_wrapper({1, 2}),
     CONCEPT.GENOMIC_FILE.SIZE: check_non_negative,
 
     # INVESTIGATOR
 
     # OUTCOME
     CONCEPT.OUTCOME.EVENT_AGE_DAYS: check_age,
-    CONCEPT.OUTCOME.DISEASE_RELATED: lambda x: (x in {
+    CONCEPT.OUTCOME.DISEASE_RELATED: null_wrapper({
         constants.OUTCOME.DISEASE_RELATED.YES,
         constants.OUTCOME.DISEASE_RELATED.NO
-    }) or (x in NULL_VALUES),
-    CONCEPT.OUTCOME.VITAL_STATUS: lambda x: (x in {
+    }),
+    CONCEPT.OUTCOME.VITAL_STATUS: null_wrapper({
         constants.OUTCOME.VITAL_STATUS.ALIVE,
         constants.OUTCOME.VITAL_STATUS.DEAD
-    }) or (x in NULL_VALUES),
+    }),
 
     # PHENOTYPE
     CONCEPT.PHENOTYPE.EVENT_AGE_DAYS: check_age,
-    CONCEPT.PHENOTYPE.OBSERVED: lambda x: (x in {
+    CONCEPT.PHENOTYPE.OBSERVED: null_wrapper({
         constants.PHENOTYPE.OBSERVED.YES,
         constants.PHENOTYPE.OBSERVED.NO
-    }) or (x in NULL_VALUES),
+    }),
 
     # READ_GROUP
     CONCEPT.READ_GROUP.LANE_NUMBER: check_positive,
-    CONCEPT.READ_GROUP.QUALITY_SCALE: lambda x: (x in {
+    CONCEPT.READ_GROUP.QUALITY_SCALE: null_wrapper({
         constants.READ_GROUP.QUALITY_SCALE.ILLUMINA13,
         constants.READ_GROUP.QUALITY_SCALE.ILLUMINA15,
         constants.READ_GROUP.QUALITY_SCALE.ILLUMINA18,
         constants.READ_GROUP.QUALITY_SCALE.SANGER,
         constants.READ_GROUP.QUALITY_SCALE.SOLEXA
-    }) or (x in NULL_VALUES),
+    }),
 
     # READ_GROUP_GENOMIC_FILE
 
@@ -171,27 +171,28 @@ INPUT_VALIDATION = {
 
     # SEQUENCING_EXPERIMENT
     CONCEPT.SEQUENCING.DATE: check_date,
-    CONCEPT.SEQUENCING.STRATEGY: lambda x: (x in {
+    CONCEPT.SEQUENCING.STRATEGY: null_wrapper({
         constants.SEQUENCING.STRATEGY.TARGETED,
         constants.SEQUENCING.STRATEGY.WXS,
         constants.SEQUENCING.STRATEGY.WGS,
         constants.SEQUENCING.STRATEGY.RNA,
         constants.SEQUENCING.STRATEGY.MRNA,
-        constants.SEQUENCING.STRATEGY.LINKED_WGS
-    }) or (x in NULL_VALUES),
-    #TODO: CONCEPT.SEQUENCING.LIBRARY_PREP
-    #TODO: CONCEPT.SEQUENCING.LIBRARY_SELECTION
-    CONCEPT.SEQUENCING.LIBRARY_STRAND: lambda x: (x in {
+        constants.SEQUENCING.STRATEGY.LINKED_WGS,
+        constants.COMMON.OTHER
+    }),
+    #TODO: CONCEPT.SEQUENCING.LIBRARY_PREP - there is no concept for this
+    #TODO: CONCEPT.SEQUENCING.LIBRARY_SELECTION - no concept for this either
+    CONCEPT.SEQUENCING.LIBRARY_STRAND: null_wrapper({
         constants.SEQUENCING.STRAND.UNSTRANDED,
         constants.SEQUENCING.STRAND.FIRST,
         constants.SEQUENCING.STRAND.SECOND,
         constants.COMMON.OTHER
-    }) or (x in NULL_VALUES),
+    }),
     CONCEPT.SEQUENCING.MAX_INSERT_SIZE: check_positive,
     CONCEPT.SEQUENCING.MEAN_DEPTH: check_positive,
     CONCEPT.SEQUENCING.MEAN_INSERT_SIZE: check_positive,
     CONCEPT.SEQUENCING.MEAN_READ_LENGTH: check_positive,
-    CONCEPT.SEQUENCING.PLATFORM: lambda x: (x in {
+    CONCEPT.SEQUENCING.PLATFORM: null_values({
         constants.SEQUENCING.PLATFORM.ION_TORRENT,
         constants.SEQUENCING.PLATFORM.SOLID,
         constants.SEQUENCING.PLATFORM.ILLUMINA,
@@ -199,13 +200,13 @@ INPUT_VALIDATION = {
         constants.SEQUENCING.PLATFORM.GENOMICS,
         constants.SEQUENCING.PLATFORM.PACBIO,
         constants.COMMON.OTHER
-    }) or (x in NULL_VALUES),
+    }),
     CONCEPT.SEQUENCING.TOTAL_READS: check_positive,
 
     # SEQUENCING_EXPERIMENT_GENOMIC_FILE
 
     # STUDY
-    CONCEPT.STUDY.RELEASE_STATUS: lambda x: (x in {
+    CONCEPT.STUDY.RELEASE_STATUS: null_wrapper({
         constants.STUDY.STATUS.FAILED,
         constants.STUDY.STATUS.PENDING,
         constants.STUDY.STATUS.PUBLISHING,
@@ -214,13 +215,13 @@ INPUT_VALIDATION = {
         constants.STUDY.STATUS.RUNNING,
         constants.STUDY.STATUS.PUBLISHED,
         constants.STUDY.STATUS.WAITING
-    }) or (x in NULL_VALUES),
+    }),
 
     # STUDY_FILE
-    CONCEPT.STUDY_FILE.AVAILABILITY: lambda x: (x in {
+    CONCEPT.STUDY_FILE.AVAILABILITY: null_wrapper({
         constants.GENOMIC_FILE.AVAILABILITY.IMMEDIATE,
         constants.GENOMIC_FILE.AVAILABILITY.COLD_STORAGE
-    }) or (x in NULL_VALUES),
+    }),
     
     # TASK
     
